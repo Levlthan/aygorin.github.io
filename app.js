be9f0905e99ea605da47582dba6933945746581a
Levlthan <nagumo233@gmail.com>
2026-02-24 23:01:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 🔹 Frases de la semana (se van acumulando día a día)
 const weeklyMessages = [
-  "Hoy te toca un dia duro y largo preciosa, seguro que tu puedes. Animo!!! Te quiero, ayer se me paso milperdones"
+  "wai un poco tarde pero guapisima mi ludopata favorita"
 ];
 
 // 🔹 Fecha y hora de inicio (hora local)
@@ -84,6 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(renderCounter, 1000); // contador en tiempo real
   setInterval(renderText, 60000);   // refresca fecha/frase cada minuto
 });
+
 
 
 
