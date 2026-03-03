067537949c6ff8479f0a5c05cc12980c12e02f24
Levlthan <nagumo233@gmail.com>
2026-03-03 15:25:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 🔹 Frases de la semana (se van acumulando día a día)
 const weeklyMessages = [
-  "Lunes de chill, lado negativo es lunes, positivo que es 2 de marzo. Que tengas un buen dia preciosa"
+  "Martes sin comer contigo, triste. Espero que vaya mejor hoy preciosa"
 ];
 
 // 🔹 Fecha y hora de inicio (hora local)
@@ -84,6 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(renderCounter, 1000); // contador en tiempo real
   setInterval(renderText, 60000);   // refresca fecha/frase cada minuto
 });
+
 
 
 
