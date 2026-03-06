348c696f29110740d506e0928320c601096ee412
Levlthan <nagumo233@gmail.com>
2026-03-06 10:36:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 🔹 Frases de la semana (se van acumulando día a día)
 const weeklyMessages = [
-  "Jueves de dormir juntitos y de cenar diferent, quieres cenar en ur home o out? invitated"
+  "Me he dormido durisimo, bueno podra ser peor. Como te va preciosa?"
 ];
 
 // 🔹 Fecha y hora de inicio (hora local)
@@ -84,6 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(renderCounter, 1000); // contador en tiempo real
   setInterval(renderText, 60000);   // refresca fecha/frase cada minuto
 });
+
 
 
 
