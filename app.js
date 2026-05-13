262885765b65717c70f3562667a2676af3ee4419
Levlthan <nagumo233@gmail.com>
2026-05-13 09:20:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 🔹 Frases de la semana (se van acumulando día a día)
 const weeklyMessages = [
-  "Yooo como has dormido princesa? Espero que bien. Te amo",
+  "Wooola como va tu dinero fscil? Bien? Aburrida? Arrepentida? Te quiero preciosa",
   
 ];
 
